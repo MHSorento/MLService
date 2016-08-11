31c3617b72135dde27dea69e50b8cc822d86ba6e
Rangoli Sharan <rangoli@Rangolis-MacBook-Pro.local>
2016-08-11 14:41:37 -0700

    JSON parsing.

---
+++ data/parse_json.py
@@ -0,0 +1,72 @@
+import json
+import numpy as np
+
+INPUT_FILE = '/Users/rangoli/Dropbox/MLService/data/dev.json'
+IMAGE_DIRECTORY = '/Users/rangoli/Dropbox/MLService/data/dev.json'
+
+def get_all_skus():
+
+	with open(INPUT_FILE) as data_file:    
+		data = json.load(data_file)
+	
+	print data.keys()
+	return data.keys()
+
+def get_all_style_tags():
+	style_tags = set()
+	with open(INPUT_FILE) as data_file:
+		data = json.load(data_file)
+		for sku, blob in data.items():
+			for style in blob["styles"]:
+				style_tags.add(style)
+
+	print list(style_tags)
+	return	list(style_tags)
+
+def get_sku_colors(skus):
+	colors = []
+	with open(INPUT_FILE) as data_file:
+		data = json.load(data_file)
+		for sku in skus:
+			color = (data[sku]["color"]["rgb"].
+				replace("r","").
+				replace("g","").
+				replace("b","").
+				replace("(","").
+				replace(")",""))
+
+			color = [float(channel)/255. for channel in color.split(",")]
+			colors.append(np.asarray(color))
+
+	colors = np.asarray(colors)
+	print colors
+	return colors
+
+def get_style_tags(skus, style_tags):
+	tags = []
+	with open(INPUT_FILE) as data_file:
+		data = json.load(data_file)
+		for sku in skus:
+			sku_tags = (data[sku]["styles"])
+			tag_vector = np.zeros([1, len(style_tags)])
+
+			for tag in sku_tags:
+				idx = style_tags.index(tag)
+				tag_vector[0, idx] = 1
+			
+			tags.append(tag_vector)
+
+	tags = np.asarray(tags)
+	print tags
+	return tags
+
+def get_image_data(skus):
+	
+def main():
+	skus = get_all_skus()
+	styles = get_all_style_tags()
+	sku_colors = get_sku_colors(skus)
+	sku_tags = get_style_tags(skus, styles)
+
+if __name__ == "__main__":
+	main()
---
+++ mlengine/vg_16_keras_minted.py
@@ -0,0 +1,77 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import numpy as np
+
+def VGG_16(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(500, activation='sigmoid'))
+
+    return model
+
+if __name__ == "__main__":
+    batch_size = 100
+    nb_epoch = 5
+
+    import time
+    start = time.time()
+    model = VGG_16()
+    duration = time.time() - start
+    print "{} s to make model".format(duration)
+    start = time.time()
+    model.output
+    duration = time.time() - start
+    print "{} s to get output".format(duration)
+    start = time.time()
+    model.compile(loss="binary_crossentropy", optimizer="sgd")
+    # model.compile(loss={"distance": "binary_crossentropy"}, optimizer="sgd")
+    duration = time.time() - start
+    print "{} s to get compile".format(duration)
+    Xtrain = np.load('input_data.npy')
+    Ytrain = np.load('ouput_labels.npy')
+    model.fit(Xtrain, Ytrain, batch_size=batch_size, nb_epoch=nb_epoch)
+    out = model.predict(Xtrain[0:1,:,:,:])
+    print out
+
